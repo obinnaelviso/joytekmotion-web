ccfac4b328246985189baf18bdf7a2afa0ea3f75
Obinna Elvis Okechukwu <dreamor47@gmail.com>
2023-05-08 14:56:06 +0100

    start website

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <nav>
+      <!-- Logo -->
+      <img src="" alt="" />
+      <!-- Menu -->
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <!-- Action Button -->
+      <button></button>
+    </nav>
+  </header>
+  <section></section>
+  <footer></footer>
+</template>
