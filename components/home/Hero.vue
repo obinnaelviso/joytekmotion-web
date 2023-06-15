d696ab973af15c032356494e8786a4524f5aa6b2
Obinna Elvis Okechukwu <dreamor47@gmail.com>
2023-06-15 12:11:43 +0100

    increase font size of slider

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -58,7 +58,7 @@ const slides = [
         <div
           class="absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center"
         >
-          <h1 class="text-lg laptop:text-4xl tablet:text-3xl font-bold mb-5" v-html="slide.title">
+          <h1 class="text-xl laptop:text-4xl tablet:text-3xl font-bold mb-5" v-html="slide.title">
           </h1>
           <!-- <Button> Get Started {{ slide }} </Button> -->
         </div>
